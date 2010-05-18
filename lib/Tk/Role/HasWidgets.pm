9b5be273e69b065d47fd29487929eca222278fb2
Jérôme Quelin <jquelin@gmail.com>
2010-05-18 17:02:48 +0200

    core of the module, taken from games::pandemic

--- lib/Tk/Role/HasWidgets.pm
+++ lib/Tk/Role/HasWidgets.pm
@@ -5,6 +5,36 @@ use warnings;
 package Tk::Role::HasWidgets;
 # ABSTRACT: keep track of your tk widgets
 
+use Moose::Role;
+use MooseX::Has::Sugar;
+
+
+=method _set_w
+
+    $object->_set_w( $name, $widget );
+
+Store a reference to C<$widget> and associate it to C<$name>.
+
+=method _w
+
+    my $widget = $object->_w( $name );
+
+Get back the C<$widget> reference associated to C<$name>.
+
+=cut
+
+# a hash to store the widgets for easier reference.
+has _widgets => (
+    ro,
+    traits  => ['Hash'],
+    isa     => 'HashRef',
+    default => sub { {} },
+    handles => {
+        _set_w => 'set',
+        _w     => 'get',
+    },
+);
+
 1;
 __END__
 
