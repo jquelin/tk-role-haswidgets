c30cb030f2fa0725539e475c9006aba685ece05f
Jérôme Quelin <jquelin@gmail.com>
2010-05-18 16:57:20 +0200

    empty stub for the module

---
+++ lib/Tk/Role/HasWidgets.pm
@@ -0,0 +1,41 @@
+use 5.010;
+use strict;
+use warnings;
+
+package Tk::Role::HasWidgets;
+# ABSTRACT: keep track of your tk widgets
+
+1;
+__END__
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEE ALSO
+
+You can look for information on this module at:
+
+=over 4
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Tk-Role-HasWidgets>
+
+=item * See open / report bugs
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Tk-Role-HasWidgets>
+
+=item * Git repository
+
+L<http://github.com/jquelin/tk-role-haswidgets>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Tk-Role-HasWidgets>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Tk-Role-HasWidgets>
+
+=back
